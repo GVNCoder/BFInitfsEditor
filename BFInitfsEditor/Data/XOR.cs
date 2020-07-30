850790860a93fd362358179985979116b535e6ef
Will Wouod <dmitrievsvp@gmail.com>
2020-07-30 18:59:52 +0300

    XOR Implementation

--- BFInitfsEditor/Data/XOR.cs
+++ BFInitfsEditor/Data/XOR.cs
@@ -1,17 +1,37 @@
-﻿using BFInitfsEditor.Service;
+﻿using System;
+using BFInitfsEditor.Service;
 
 namespace BFInitfsEditor.Data
 {
     public class XOR : IXOR
     {
-        public byte[] Encrypt(byte[] data, byte[] key)
+        public static IXOR GetInstance() => new XOR();
+
+        // private ctor
+        private XOR() { }
+
+        #region IXOR
+
+        public byte[] Encrypt(byte[] data, byte[] key) => _XOR(data, key);
+        public byte[] Decrypt(byte[] data, byte[] key) => _XOR(data, key);
+
+        #endregion
+
+        #region Private helpers
+
+        private byte[] _XOR(byte[] data, byte[] key)
         {
-            return new byte[1]; // TODO: dummy
+            var result = new byte[data.Length];
+            Array.Copy(data, result, data.Length);
+
+            for (var i = 0; i < data.Length; i++)
+            {
+                result[i] ^= (byte)(key[i % 257] ^ 0x7B);
+            }
+
+            return result;
         }
 
-        public byte[] Decrypt(byte[] data, byte[] key)
-        {
-            return new byte[1]; // TODO: dummy
-        }
+        #endregion
     }
 }
