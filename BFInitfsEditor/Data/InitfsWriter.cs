a5e0b1746f515aca9134df640d7fbfb476bda6cd
Will Wouod <dmitrievsvp@gmail.com>
2020-08-01 14:10:27 +0300

    Base writer implementation

--- BFInitfsEditor/Data/InitfsWriter.cs
+++ BFInitfsEditor/Data/InitfsWriter.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Text;
 
+using BFInitfsEditor.Extension;
 using BFInitfsEditor.Model;
 using BFInitfsEditor.Service;
 
@@ -15,71 +15,87 @@ namespace BFInitfsEditor.Data
         private readonly IXOR _xor;
         private readonly ILeb128 _leb128;
         private readonly byte[] _eof = { 0x00, 0x00 };
+        private readonly byte[] _zeros = new byte[30];
 
         // private ctor 
         private InitfsWriter()
         {
             _xor = XOR.GetInstance();
             _leb128 = Leb128.GetInstance();
+            _zeros.Fill<byte>(0x00);
         }
 
         #region IInitfsWriter
 
         public void Write(Stream target, Entity entity)
         {
-            var entityData = entity.Data;
+            var entityDataRef = entity.Data;
             byte[] data = null;
 
-            // prepare files for writing
-            using (var dataBlock = new MemoryStream())
+            // prepare output initfs_ file data
+            using (var dataStream = new MemoryStream())
             {
-                // write full data size
-                var dataSize = _leb128.BuildLEB128Unsigned(entityData.DataSize);
-                dataBlock.Write(dataSize, 0, dataSize.Length);
+                // write header + 4 zero bytes
+                dataStream.Write(entity.Header, 0, entity.Header.Length);
+                dataStream.Write(_zeros, 0, Entity.AfterHeaderZero);
 
-                // handle all file entries
-                foreach (var entry in entityData.Entries)
+                // write hash + 30 zero bytes
+                dataStream.Write(entity.Hash, 0, entity.Hash.Length);
+                dataStream.Write(_zeros, 0, Entity.AfterHashZero);
+
+                // write encryption key + 3 zero bytes
+                dataStream.Write(entity.EncryptionKey, 0, entity.EncryptionKey.Length);
+                dataStream.Write(_zeros, 0, Entity.AfterEncryptionKeyZero);
+                
+                byte[] entriesStreamData = null;
+
+                // prepare file entries data
+                using (var entriesStream = new MemoryStream())
                 {
-                    byte[] entryBytes = null;
+                    // write full data size
+                    var dataSize = _leb128.BuildLEB128Unsigned(entityDataRef.DataSize);
+                    entriesStream.Write(dataSize, 0, dataSize.Length);
 
-                    using (var memorySteam = new MemoryStream())
+                    foreach (var entry in entityDataRef.Entries)
                     {
                         // write unknown data
                         var unknownSize = _leb128.BuildLEB128Unsigned(entry.UnknownSize);
-                        memorySteam.Write(unknownSize, 0, unknownSize.Length);
-                        memorySteam.Write(entry.UnknownHeaderPart, 0, entry.UnknownHeaderPart.Length);
+                        entriesStream.Write(unknownSize, 0, unknownSize.Length);
+                        entriesStream.Write(entry.UnknownHeaderPart, 0, entry.UnknownHeaderPart.Length);
 
                         // write type name string
                         var typeString = Encoding.ASCII.GetBytes(entry.Type).Concat(new byte[] { 0x00 }).ToArray();
-                        memorySteam.Write(typeString, 0, typeString.Length);
+                        entriesStream.Write(typeString, 0, typeString.Length);
 
                         // write entry struct size + entry header
                         var structSize = _leb128.BuildLEB128Unsigned(entry.StructSize);
-                        memorySteam.Write(structSize, 0, structSize.Length);
-                        memorySteam.Write(entry.UnknownData, 0, entry.UnknownData.Length);
+                        entriesStream.Write(structSize, 0, structSize.Length);
+                        entriesStream.Write(entry.UnknownData, 0, entry.UnknownData.Length);
                         
                         // write file size + file data + eof
                         var fileSize = _leb128.BuildLEB128Unsigned(entry.FileSize);
-                        memorySteam.Write(fileSize, 0, fileSize.Length);
-                        memorySteam.Write(entry.FileData, 0, entry.FileData.Length);
-                        memorySteam.Write(_eof, 0, _eof.Length);
-
-                        // extract payload bytes
-                        entryBytes = memorySteam.ToArray();
+                        entriesStream.Write(fileSize, 0, fileSize.Length);
+                        entriesStream.Write(entry.FileData, 0, entry.FileData.Length);
+                        entriesStream.Write(_eof, 0, _eof.Length);
                     }
 
-                    // write entry bytes
-                    dataBlock.Write(entryBytes, 0, entryBytes.Length);
+                    entriesStream.Write(new byte[] { 0x00 }, 0, 1);
+
+                    // extract recorded data
+                    entriesStreamData = entriesStream.ToArray();
                 }
 
-                // extract payload bytes
-                data = dataBlock.ToArray().Concat(new byte[] { 0x00 }).ToArray();
+                // encrypt and write entries data
+                var encryptedEntries = _xor.Encrypt(entriesStreamData, entity.EncryptionKey);
+                dataStream.Write(encryptedEntries, 0, encryptedEntries.Length);
+
+                // extract recorded data
+                data = dataStream.ToArray();
             }
 
             // write constructed initfs_ to file
             using (var writer = new BinaryWriter(target))
             {
-                // TODO: Write here
                 writer.Write(data);
             }
         }
---
+++ BFInitfsEditor/Extension/ArrayExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace BFInitfsEditor.Extension
+{
+    public static class ArrayExtension
+    {
+        public static void Fill<T>(this T[] target, T with)
+        {
+            for (var i = 0; i < target.Length; i++)
+                target[i] = with;
+        }
+    }
+}
